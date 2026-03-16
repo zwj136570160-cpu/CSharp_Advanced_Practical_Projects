5bb5ca3b86973aa1db548e284a2e0f4a58115cca
ZWJ136570160-CPU <zwj136570160@gmail.com>
2026-03-16 17:37:52 +0800

    添加项目文件。

---
+++ CSharp进阶实践项目/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp进阶实践项目
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
